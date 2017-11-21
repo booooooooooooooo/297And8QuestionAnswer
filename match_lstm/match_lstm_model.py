ab6d057e385f8fc37750bd2e750e307a750e0011
bo <bo@bos-macbook-pro.lan>
2017-11-20 20:54:19 -0800

    LSTM_encoderDone

--- match_lstm/match_lstm_model.py
+++ match_lstm/match_lstm_model.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 class LSTM_encoder:
     def __init__(self, cellName, input_size, state_size):
@@ -26,17 +27,16 @@ class LSTM_encoder:
     def encode_one_step(self, batch_inputs, batch_states, batch_memories):
         i_k = tf.nn.sigmoid(tf.matmul(batch_inputs, self.W_i) + tf.matmul(batch_states, self.V_i) + self.b_i)
         f_k = tf.nn.sigmoid(tf.matmul(batch_inputs, self.W_f) + tf.matmul(batch_states, self.V_f) + self.b_f)
-        o_k = tf.nn.sigmoid(tf.matmul(batch_inputs, self.W_o) + tf.matmul(batch_inputs, self.V_o) + self.b_o)
+        o_k = tf.nn.sigmoid(tf.matmul(batch_inputs, self.W_o) + tf.matmul(batch_states, self.V_o) + self.b_o)
         new_batch_memories = f_k * batch_memories + i_k * tf.nn.tanh( tf.matmul(batch_inputs, self.W_c) + tf.matmul(batch_states, self.V_c) + self.b_c)
         new_batch_states = o_k * tf.nn.tanh(new_batch_memories)
 
         return new_batch_states, new_batch_memories
-    def encode_sequence(self, batch_inputs_sequence):
+    def encode_sequence(self, batch_inputs_sequence, seq_len):
         # Assume all sequences have same length. Padding work is done in data related files.
         states = []
-        seq_len = batch_inputs_sequence.shape[1]
-        batch_states = tf.zeros( (batch_inputs_sequence.shape[0], self.state_size) ) #TODO: problem of None
-        batch_memories = tf.zeros( (batch_inputs_sequence.shape[0], self.state_size) ) #TODO: problem of None
+        batch_states = tf.zeros( ( tf.shape(batch_inputs_sequence)[0], self.state_size) ) #TODO: problem of None
+        batch_memories = tf.zeros( ( tf.shape(batch_inputs_sequence)[0], self.state_size) ) #TODO: problem of None
         for i in xrange(seq_len):
             new_batch_states, new_batch_memories = self.encode_one_step(batch_inputs_sequence[:,i], batch_states, batch_memories)
             states.append(new_batch_states)
@@ -46,18 +46,19 @@ class LSTM_encoder:
 def sanity_LSTM_encoder():
     input_size = 3
     state_size = 4
+    seq_len = 5
 
-    inputs_placeholder = tf.placeholder(tf.float32, shape=(None,input_size))
+    inputs_placeholder = tf.placeholder(tf.float32, shape=(None, seq_len, input_size))
     encoder = LSTM_encoder("test", input_size, state_size)
     encoder.add_variables()
-    predicted = encoder.encode_sequence(inputs_placeholder)
+    predicted = encoder.encode_sequence(inputs_placeholder, seq_len)
+
 
     sess = tf.Session()
-
-    session.run( tf.global_variables_initializer() )
-    inputs = tf.zeros((10, input_size))
-    labels = tf.zeros((10, input_size, state_size))
-    assert np.allclose(labels, sess.run(predicted, {inputs_placeholder : inputs}))
+    sess.run( tf.global_variables_initializer() )
+    inputs = np.zeros((10, seq_len, input_size))
+    print sess.run(  tf.shape( sess.run(predicted, {inputs_placeholder : inputs}) )  )
+    print sess.run(predicted, {inputs_placeholder : inputs})
 
 if __name__ == "__main__":
     sanity_LSTM_encoder()
